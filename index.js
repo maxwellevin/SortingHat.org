68d94f0f4eae85ded0362689815454c3a48c20ad
Maxwell <msl@lclark.edu>
2019-04-30 18:20:38 -0700

    Handled file opening and initial parsing via PapaParse.

--- index.js
+++ index.js
@@ -2,26 +2,35 @@
 
 window.onload = function () {
 
-    /** LARS: Feel free to change the names of these functions. I'm just writing stubs to get organized for now. - Max*/
     // Add event listeners for "Choose File" buttons
-    document.getElementById("upload_student").onclick = handleStudentFile;
-    document.getElementById("upload_section").onclick = handleSectionFile;
+    document.getElementById("upload_student").onchange = handleStudentFile;
+    document.getElementById("upload_section").onchange = handleSectionFile;
 
 
     /** Handles the student csv file uploading. */
     function handleStudentFile() {
-        let file = chooseFile();
-        return -1;
+        let file = document.getElementById("upload_student").files[0];
+        Papa.parse(file, {  // This is asynchronous so how do we actually deal with this?
+            // step: function(results) {
+            //     console.log("Row:", results.data);
+            // },
+            complete: function (results) {
+                // All the magic happens here
+                console.log("Finished");
+            }
+        });
     }
+
 
     /** Handles the section csv file uploading. */
     function handleSectionFile() {
-        let file = chooseFile();
-        return -1;
-    }
-
-    /** Handles user interaction with opening a new file. Maybe returns the path to the file, or actually loads the file in memory.*/
-    function chooseFile() {
+        let file = document.getElementById("upload_section").files[0];
+        Papa.parse(file, {  // This is asynchronous so how do we actually deal with this?
+            complete: function (results) {
+                // All the magic happens here
+            }
+        });
+        console.log("Picked Section File");
         return -1;
     }
 
@@ -31,13 +40,15 @@ window.onload = function () {
         return document.getElementById("female_ratio_input").value / 100;
     }
 
+
     /** Returns the maximum permitted male ratio in any section from the input slider.*/
     function getMaleRatioInput() {
         return document.getElementById("male_ratio_input").value / 100;
     }
 
+
     /** Returns the maximum permitted athlete ratio in any section from the input slider.*/
     function getAthleteRatioInput() {
         return document.getElementById("athlete_ratio_input").value / 100;
     }
-}
+};
