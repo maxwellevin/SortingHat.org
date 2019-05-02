abfef0b15d2cc8a3215f2b0b4a32aa54f8dddbc8
Maxwell <msl@lclark.edu>
2019-05-01 18:52:48 -0700

    Now successfully parses csv inputs. Also added TODOs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ window.onload = function () {
     /** Add event listeners for "Choose File" buttons */
     document.getElementById("upload_student").onchange = handleStudentFile;
     document.getElementById("upload_section").onchange = handleSectionFile;
+    document.getElementById("run").onclick = runProgram;
 
 
     /** Boolean values to track handling of students and sections. */
@@ -12,17 +13,29 @@ window.onload = function () {
     let sectionsHandled = false;
 
 
+    /** Arrays to keep track of the students and sections data parsed from csv inputs. */
+    let studentsData = [];
+    let sectionsData = [];
+
+
     /** Handles the student csv file uploading. */
     function handleStudentFile() {
         studentsHandled = false;
         handleRunButton();
         let file = document.getElementById("upload_student").files[0];
+        let arr = [];
         Papa.parse(file, {
+            header: true,
             step: function (results) {
-                // TODO: Build students array
-                console.log("Row:", results.data);
+                if(results.errors.length > 0) {
+                    // TODO: Better handle errors
+                    console.log("ERRORS:", results.errors);
+                    return;
+                }
+                arr.push(results.data[0]);
             },
-            complete: function (results) {
+            complete: function (results, file) {
+                studentsData = arr;
                 studentsHandled = true;
                 handleRunButton();  // checks to see if sections are handled too
             }
@@ -35,17 +48,22 @@ window.onload = function () {
         sectionsHandled = false;
         handleRunButton();
         let file = document.getElementById("upload_section").files[0];
+        let arr = [];
         Papa.parse(file, {
             step: function (results) {
-                // TODO: Build sections array
-                console.log("Row:", results.data);
+                if(results.errors.length > 0) {
+                    // TODO: Better handle errors
+                    console.log("ERRORS:", results.errors);
+                    return;
+                }
+                arr.push(results.data[0]);
             },
-            complete: function (results) {
+            complete: function (results, file) {
                 sectionsHandled = true;
+                sectionsData = arr;
                 handleRunButton();  // checks to see if students are handled too
             }
         });
-        console.log("Picked Section File");
     }
 
 
@@ -59,20 +77,40 @@ window.onload = function () {
     }
 
 
-    /** Returns the maximum permitted female ratio in any section from the input slider.*/
+    /** Launches the meat of the program and reports the results. */
+    function runProgram() {
+        console.log("Starting to run");
+        console.log(studentsData)
+        // TODO: Compute size of the cost matrix & optionally handle obvious errors
+        // TODO: Implement mapping of column index (seat) to section
+        // TODO: Implement construction of cost matrix
+        // TODO: Implement or borrow hungarian/munkres algorithm for allocations
+        // TODO: Retrieve allocations from algorithm
+        // TODO: Generate a report of the program's performance
+        // TODO: Activate the save button
+    }
+
+
+    /** Returns the maximum permitted female ratio in any section from the input slider. */
     function getFemaleRatioInput() {
         return document.getElementById("female_ratio_input").value / 100;
     }
 
 
-    /** Returns the maximum permitted male ratio in any section from the input slider.*/
+    /** Returns the maximum permitted male ratio in any section from the input slider. */
     function getMaleRatioInput() {
         return document.getElementById("male_ratio_input").value / 100;
     }
 
 
-    /** Returns the maximum permitted athlete ratio in any section from the input slider.*/
+    /** Returns the maximum permitted athlete ratio in any section from the input slider. */
     function getAthleteRatioInput() {
         return document.getElementById("athlete_ratio_input").value / 100;
+    }
+
+
+    /** Saves the results of the program upon a successful run of the algorithm. */
+    function saveResults() {
+        // TODO: Save the results to a csv file of the user's designation
     }
 };
