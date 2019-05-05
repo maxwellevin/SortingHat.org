74eb8fdd9f74cf3b8412c96921b5ad3571820613
Maxwell <msl@lclark.edu>
2019-05-04 21:54:10 -0700

    Now considers pre-allocated students

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 window.onload = function () {
 
-    /** Add event listeners for "Choose File" buttons */
+    /** Add event listeners for actionable buttons */
     document.getElementById("upload_student").onchange = handleStudentFile;
     document.getElementById("upload_section").onchange = handleSectionFile;
     document.getElementById("run").onclick = runProgram;
+    document.getElementById("save_as").onclick = saveResults;
 
 
     /** Boolean values to track handling of students and sections. */
@@ -13,9 +14,20 @@ window.onload = function () {
     let sectionsHandled = false;
 
 
-    /** Arrays to keep track of the students and sections data parsed from csv inputs. */
-    let studentsData = [];
-    let sectionsData = [];
+    /** Objects to keep track of the students and sections data parsed from csv inputs. */
+    let initialStudentsData = {};  // ID: "ID"
+    let initialSectionsData = {};  // ID: "Core Section #"
+
+
+    /** Objects to keep track of the students and sections data filtered from the original inputs. These are used as
+     * inputs in the hungarian/munkres algorithm. */
+    let studentsData = {};  // ID: "ID"
+    let sectionsData = {};  // ID: "Core Section #"
+
+
+    /** The sum of the class limits minus the number of pre-assigned students. This is the number of columns in the cost
+     * matrix, each of which represents a "job" for the hungarian algorithm to allocate. */
+    let numAvailableSeats;
 
 
     /** Handles the student csv file uploading. */
@@ -23,19 +35,22 @@ window.onload = function () {
         studentsHandled = false;
         handleRunButton();
         let file = document.getElementById("upload_student").files[0];
-        let arr = [];
+        let arr = {};
         Papa.parse(file, {
             header: true,
             step: function (results) {
-                if(results.errors.length > 0) {
+                if (results.errors.length > 0) {
                     // TODO: Better handle errors
                     console.log("ERRORS:", results.errors);
+                    alert("An error occurred while handling the student csv file: " + results.errors);
                     return;
                 }
-                arr.push(results.data[0]);
+                let student = results.data[0];
+                arr[student["ID"]] = student;
             },
             complete: function (results, file) {
-                studentsData = arr;
+                console.log(results);
+                initialStudentsData = arr;
                 studentsHandled = true;
                 handleRunButton();  // checks to see if sections are handled too
             }
@@ -48,19 +63,23 @@ window.onload = function () {
         sectionsHandled = false;
         handleRunButton();
         let file = document.getElementById("upload_section").files[0];
-        let arr = [];
+        let arr = {};
         Papa.parse(file, {
+            header: true,
+            dynamicTyping: true,
             step: function (results) {
-                if(results.errors.length > 0) {
+                if (results.errors.length > 0) {
                     // TODO: Better handle errors
                     console.log("ERRORS:", results.errors);
+                    alert("An error occurred while handling the sections csv file: " + results.errors);
                     return;
                 }
-                arr.push(results.data[0]);
+                let section = results.data[0];
+                arr[section["Core Section #"]] = section;
             },
             complete: function (results, file) {
                 sectionsHandled = true;
-                sectionsData = arr;
+                initialSectionsData = arr;
                 handleRunButton();  // checks to see if students are handled too
             }
         });
@@ -79,8 +98,21 @@ window.onload = function () {
 
     /** Launches the meat of the program and reports the results. */
     function runProgram() {
-        console.log("Starting to run");
-        console.log(studentsData)
+        document.getElementById("run").disabled = true;
+        console.log("Read input data:");
+        // console.log(initialStudentsData);
+        // console.log(initialSectionsData);
+
+        console.log(Object.keys(initialStudentsData));
+
+        // These are filtered versions of the original data where pre-assigned students are removed from the students
+        // array and sections are updated to reflect the student cap with pre-assigned students.
+        console.log("Filtering input data:");
+        filterInputData();
+        // console.log(studentsData);
+        // console.log(sectionsData);
+
+        // TODO: Handle pre-assigned students
         // TODO: Compute size of the cost matrix & optionally handle obvious errors
         // TODO: Implement mapping of column index (seat) to section
         // TODO: Implement construction of cost matrix
@@ -88,6 +120,44 @@ window.onload = function () {
         // TODO: Retrieve allocations from algorithm
         // TODO: Generate a report of the program's performance
         // TODO: Activate the save button
+    }
+
+
+    /** Filters the original students and sections data to account for pre-assigned students and students with illegal
+     * preferences. Returns an array containing an array of students and an array of sections. */
+    function filterInputData() {
+        studentsData = [];
+        sectionsData = Object.assign([], initialSectionsData);
+        let studentKeys = Object.keys(initialStudentsData);
+        // console.log(studentKeys);
+        // console.log(initialStudentsData);
+        Object.keys(initialStudentsData).forEach(function (key, index) {
+            console.log(key);
+            let student = initialStudentsData[key];
+            // console.log(student);
+            if (student["Placement"] !== "") {  // Student has already been assigned a section
+                let sectionKey = student["Placement"];
+                sectionsData[sectionKey]["Student Cap"] -= 1;
+            } else {  // Student needs to be assigned a section, so they get copied
+                studentsData[key] = Object.assign([], student);
+            }
+        });
+    }
+
+
+    /** Constructs the cost matrix based on user-input parameters and the students/sections arrays. Returns a matrix of
+     * weights which represent the cost of assigning the student (represented by a row) to a seat in a class
+     * (represented by individual columns, stacked sequentially). */
+    function buildCostMatrix() {
+        /* Define parameters for the cost matrix based on user-defined configurations. */
+        let alpha = 3.5;
+        let defaultWeight = Math.pow(alpha, 7);
+        let illegalWeight = Math.pow(alpha, 9);
+        let minFemaleRatio = 1 - getFemaleRatioInput();
+        let minMaleRatio = 1 - getMaleRatioInput();
+        let minNonAthleteRatio = 1 - getAthleteRatioInput();
+
+
     }
 
 
