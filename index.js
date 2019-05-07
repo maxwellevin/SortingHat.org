e3e7353cb5fceee551d1b0896acfdcf947b2db48
Maxwell <msl@lclark.edu>
2019-05-06 18:58:26 -0700

    Implemented method to build an array of seat objects.

--- index.js
+++ index.js
@@ -104,9 +104,12 @@ window.onload = function () {
         console.log(preassignedStudents);
 
 
-        // TODO: Handle pre-assigned students
+        let seats = buildSeatObjects();
+        console.log("The following seats have been prepared:");
+        console.log(seats);
+
         // TODO: Compute size of the cost matrix & optionally handle obvious errors
-        // TODO: Implement mapping of column index (seat) to section
+        // TODO: Implement mapping of column index (seat) to section // DONE: Column index = seat index
         // TODO: Implement construction of cost matrix
         // TODO: Implement or borrow hungarian/munkres algorithm for allocations
         // TODO: Retrieve allocations from algorithm
@@ -143,6 +146,53 @@ window.onload = function () {
         return preassignedStudents;
     }
 
+
+    /** Builds an object to hold an array of seats. Each seat has three main properties:
+     * reserved: true if the seat is reserved for a specific gender of student, false otherwise.
+     * gender: Either "M", "F", or "". Indicates the gender of student that the seat is reserved for.
+     * section: A string which identifies the section this seat belongs to. */
+    function buildSeatObjects() {
+        // Define a seats array
+        let seatsArray = [];
+
+        // Iterate through all of the sections (I hope this does so in order, otherwise we'll have issues later)
+        Object.keys(sectionsData).forEach(function (key, _) {
+            let currentSection = sectionsData[key];
+            let numSeats = currentSection["Student Cap"];
+
+            // Reserve a number of seats for males:
+            let numMaleSeats = Math.round(numSeats * (1 - getMaleRatioInput()));
+            for (let i = 0; i < numMaleSeats; i++) {
+                seatsArray.push({
+                    reserved: true,
+                    gender: "M",
+                    section: currentSection
+                });
+            }
+
+            // Reserve a number of seats for females
+            let numFemaleSeats = Math.round(numSeats * (1 - getFemaleRatioInput()));
+            for (let i = 0; i < numFemaleSeats; i++) {
+                seatsArray.push({
+                    reserved: true,
+                    gender: "F",
+                    section: currentSection
+                });
+            }
+
+            // Push the final seats to the seatsArray
+            for (let i = 0; i < numSeats - numMaleSeats - numFemaleSeats; i++) {
+                seatsArray.push({
+                    reserved: false,
+                    gender: "",
+                    section: currentSection
+                });
+            }
+        });
+
+        // Return the seats array
+        return seatsArray;
+    }
 
     /** Constructs the cost matrix based on user-input parameters and the students/sections arrays. Returns a matrix of
      * weights which represent the cost of assigning the student (represented by a row) to a seat in a class
