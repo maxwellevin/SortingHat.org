2ae76aacf1ccc1c5201dd71ec1b403be52ea59d3
Maxwell <msl@lclark.edu>
2019-08-04 09:34:40 -0700

    seat allocations account for gender slider change. simplified handleRunButton().

--- index.js
+++ index.js
@@ -100,11 +100,7 @@ window.onload = function () {
     /** Toggles the state of the 'run' button based on the states of studentsHandled and sectionsHandled. */
     function handleRunButton() {
         document.getElementById("save_as").disabled = true;
-        if (studentsHandled && sectionsHandled) {
-            document.getElementById("run").disabled = false;
-        } else {
-            document.getElementById("run").disabled = true;
-        }
+        document.getElementById("run").disabled = !(studentsHandled && sectionsHandled);
     }
 
 
@@ -133,6 +129,7 @@ window.onload = function () {
 
         // TODO: Generate a report of the gender/athlete balances, smallest classes, most popular class choice, etc.
         let report = createReport();
+        printReport(report);
 
         document.getElementById("run").disabled = false;
         document.getElementById("save_as").disabled = false;
@@ -182,7 +179,7 @@ window.onload = function () {
             let numSeats = currentSection["Student Cap"];
 
             // Reserve a number of seats for males:
-            let numMaleSeats = Math.round(numSeats * (1 - getMaleRatioInput()));
+            let numMaleSeats = Math.round(numSeats * getMaleRatioInput());
             for (let i = 0; i < numMaleSeats; i++) {
                 seatsArray.push({
                     reserved: true,
@@ -192,7 +189,7 @@ window.onload = function () {
             }
 
             // Reserve a number of seats for females
-            let numFemaleSeats = Math.round(numSeats * (1 - getFemaleRatioInput()));
+            let numFemaleSeats = Math.round(numSeats * getFemaleRatioInput());
             for (let i = 0; i < numFemaleSeats; i++) {
                 seatsArray.push({
                     reserved: true,
